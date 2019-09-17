2e0479b3d38016e41c5c09481686e8571a52df35
lawrencefmm <lawrencefmm@gmail.com>
2019-09-17 12:35:48 -0300

    added solution

---
+++ CODEFORCES/791A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cin >>a >>b;
+	c=0;
+ while(a<=b){
+   a=a*3;
+   b=b*2;
+   c=c+1;
+
+ }
+ 	cout <<c;
+
+
+
+	return 0;
+}
---
+++ CODEFORCES/817A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t=0;
+	int x1,x2,y1,y2,x,y;
+
+	cin >> x1 >> y1 >> x2 >> y2 >> x >> y;
+	int i=y2-y1;
+	int j=x2-x1;
+
+	if(i%y==0 and j%x==0 and (j/x+i/y)%2==0){
+		cout<< "YES";
+
+	}else{
+		cout << "NO";
+	}
+
+
+
+
+
+	return 0;
+
+}
